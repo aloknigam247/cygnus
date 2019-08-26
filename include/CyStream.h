75a08ea90aa2b62210af2bdbfe5f15582ead1429
aloknigam247 <aloknigam247@gmail.com>
2019-08-26 21:59:41 +0530

    added required features

---
+++ include/CyStream.h
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <string>
+
+class CyStream {
+    std::fstream f;
+    bool isOpen;
+
+    public:
+    void open(const std::string &filename, std::ios_base::openmode mode = std::ios_base::in|std::ios_base::out);
+    void close();
+    ~CyStream();
+};
