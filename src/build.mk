23d173bf4067b73a9ae6901f27d3b210e6eecbc0
aloknigam247 <aloknigam247@gmail.com>
2019-09-08 11:09:37 +0530

    adding coverage build

--- src/build.mk
+++ src/build.mk
@@ -6,6 +6,6 @@ DEP_FILES = $(addprefix $(DEP)/,$(SRC_FILES:.cc=.d))
 objs: $(OBJ_FILES)
 
 $(OBJ_FILES):
-	$(CPP_COMPILE) -c -o $@ $<
+	$(CPP_COMPILE) $(INCLUDE_DIR) -c -o $@ $<
 
 include $(DEP_FILES)
