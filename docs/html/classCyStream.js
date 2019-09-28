53242a9f73ff8e9f5dfcf153a833c69f7ac97ca1
aloknigam247 <aloknigam247@gmail.com>
2019-09-28 21:46:07 +0530

    cleanup

--- docs/html/classCyStream.js
+++
@@ -1,6 +0,0 @@
-var classCyStream =
-[
-    [ "~CyStream", "classCyStream.html#a10ae315b0c29863ff87b7da147cc10d8", null ],
-    [ "close", "classCyStream.html#a5b701eed8d68ecedc62eac45a2dfc916", null ],
-    [ "open", "classCyStream.html#a14a144c027311f5f470babd43a302b4c", null ]
-];
--- docs/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['log',['Log',['../classLog.html',1,'']]]
-];
