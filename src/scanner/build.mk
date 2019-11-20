ed100d9ea561cf8e14cdac5b1384bbab82fe2058
aloknigam247 <aloknigam247@gmail.com>
2019-11-20 15:41:06 +0530

    added scanner module and docs retention

---
+++ src/scanner/build.mk
@@ -0,0 +1,13 @@
+SRC_FILES = $(wildcard *.cpp)
+OBJ_FILES = $(addprefix $(OBJ)/,$(SRC_FILES:.cpp=.o))
+DEP_FILES = $(addprefix $(DEP)/,$(SRC_FILES:.cpp=.d))
+
+all: $(OBJ_FILES) $(OBJ_FILES)
+
+$(OBJ_FILES):
+	$(CY_COMPILE) $(INCLUDE_DIR) -c -o $@ $<
+ifeq ($(MODE),coverage)
+	ln -fsr $< $(OBJ)/
+endif
+
+include $(DEP_FILES)
