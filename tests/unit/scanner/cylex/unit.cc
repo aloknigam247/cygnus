53fa4e38057d4684c3ab3f6f19b1c2aec8198e9f
aloknigam247 <aloknigam247@gmail.com>
2020-03-15 08:42:22 +0530

    complete cylex.cyt coverage

--- tests/unit/scanner/cylex/unit.cc
+++ tests/unit/scanner/cylex/unit.cc
@@ -1,21 +1,14 @@
 #include "cylex.h"
 
-#include <chrono>
-
 #include "cytest.h"
 
 void analyseFile() {
     CyLex lex;
-    auto start = std::chrono::system_clock::now();
 
     lex.addPattern("int");
     lex.addPattern("float");
     lex.readFile("text");
     lex.analyse();
-
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-//    Log::i("elapsed time: ", std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed_seconds).count());
 }
 
 int main() {
